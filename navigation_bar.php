b5fd57d5b4c1a5e4a5b3df1e710050a038aef746
abiboy <busy_bizi15@ymail.com>
2016-09-02 00:50:59 +0200

    Completed the register page and login page.
    Added bootstrap Styling to the pages.

--- navigation_bar.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '
-<ul class="nav_bar">	
-<li><a href="#">Home</a></li>	
-<li><a href="#">About Us</a></li>	
-<li><a href="#">Register</a></li>	
-<li><a href="#">Link</a></li>
-</ul';
-?>
