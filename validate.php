99f6b9ee60ae7828ef8d065aca9e3ad9a62f6a72
abiboy <busy_bizi15@ymail.com>
2016-09-01 09:50:36 +0200

    Added $_POST values and "if"
    Filled the blank post superglobals and added an if statement to check
    which type of request method.

--- validate.php
+++ validate.php
@@ -1,17 +1,18 @@
 <?php
 $fname = $lname = $dob = $prolev = $phone = $email = $raddress = $postaddress = $kinname = $kinemail = $kinphone = "";
 
-$fname = test_form($_POST[""]);
-$lname = test_form($_POST[""]);
-$dob = test_form($_POST[""]);
-$prolev = test_form($_POST[""]);
-$phone = test_form($_POST[""]);
-$email = test_form($_POST[""]);
-$raddress = test_form($_POST[""]);
-$postaddress = test_form($_POST[""]);
-$kinname = test_form($_POST[""]);
-$kinemail = test_form($_POST[""]);
-$kinphone = test_form($_POST[""]);
+if ($_SERVER["REQUEST_METHOD"] == )
+$fname = test_form($_POST["FNAME"]);
+$lname = test_form($_POST["LNAME"]);
+$dob = test_form($_POST["DOB"]);
+$prolev = test_form($_POST["PROLEV"]);
+$phone = test_form($_POST["PHONE"]);
+$email = test_form($_POST["EMAIL"]);
+$raddress = test_form($_POST["RADDRESS"]);
+$postaddress = test_form($_POST["POSTADDRESS"]);
+$kinname = test_form($_POST["KINNAME"]);
+$kinemail = test_form($_POST["KINEMAIL"]);
+$kinphone = test_form($_POST["KINPHONE"]);
 
 
 function test_form($data){
